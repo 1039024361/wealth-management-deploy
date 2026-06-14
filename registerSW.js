92d346e7e19a5ed991a90fa9e81332c18a390eb5
zhongderer <1039024361@qq.com>
2026-06-15 00:28:19 +0800

    add bias path

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wealth-management-deploy/sw.js', { scope: '/wealth-management-deploy/' })})}
